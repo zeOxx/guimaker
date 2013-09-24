a3afcc4ce29fbedb582c4ff4d8f28edbea7bbcb9
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2013-09-24 18:04:40 +0200

    trololol

---
+++ src/no/whg/GUIMaker/GUIMaker.java
@@ -0,0 +1,14 @@
+package no.whg.GUIMaker;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: snorre
+ * Date: 9/24/13
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GUIMaker {
+    public static void main (String args){
+        // herbaderb
+    }
+}

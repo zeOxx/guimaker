620bee0d433e6f606b9b86ebefdcffbcea3a3afd
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2013-09-24 20:26:10 +0200

    added saving/loading of layout

--- src/no/whg/GUIMaker/GUIMaker.java
+++ src/no/whg/GUIMaker/GUIMaker.java
@@ -1,5 +1,10 @@
 package no.whg.GUIMaker;
 
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import java.io.Serializable;
+
 /**
  * Created with IntelliJ IDEA.
  * User: snorre
@@ -8,7 +13,56 @@ package no.whg.GUIMaker;
  * To change this template use File | Settings | File Templates.
  */
 public class GUIMaker {
-    public static void main (String args){
-        // herbaderb
+    public ArrayList rowList = new ArrayList<rowElement>();
+
+    public static void main (String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createUI();
+            }
+        });
     }
+
+    static void createUI (){
+
+    }
+
+    public boolean loadGUI (File f){
+        Object object = null;
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            this.rowList = (ArrayList<rowElement>)ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException e) {
+            return false;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean saveGUI (File f){
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(rowList);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void previewGUI (){
+        //previewWindow = new previewWindow; previewWindow.run; or something
+    }
+
+    public void saveSource (){
+
+    }
+
+
 }
---
+++ src/no/whg/GUIMaker/rowElement.java
@@ -0,0 +1,15 @@
+package no.whg.GUIMaker;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: snorre
+ * Date: 9/24/13
+ * Time: 7:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class rowElement implements Serializable {
+    //bla
+    private static final long serialVersionUID = 1L;
+}

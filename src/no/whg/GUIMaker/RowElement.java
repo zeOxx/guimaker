5f36de856847eea8c24d17ea832399a4e7ae2c23
Snorre Brecheisen <snotten@gmail.com>
2013-10-02 19:33:33 +0200

    fixed header

--- src/no/whg/GUIMaker/RowElement.java
+++ src/no/whg/GUIMaker/RowElement.java
@@ -3,13 +3,12 @@ package no.whg.GUIMaker;
 import java.io.Serializable;
 
 /**
- * Created with IntelliJ IDEA.
+ * Created with IntelliJ IDEA for ${PROJECT_NAME}
  * User: snorre
  * Date: 9/24/13
  * Time: 7:31 PM
- * To change this template use File | Settings | File Templates.
  */
 public class RowElement implements Serializable {
-    //bla
+
     private static final long serialVersionUID = 1L;
 }

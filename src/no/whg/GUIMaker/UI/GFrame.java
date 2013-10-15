67c7ee98e6454565ded707f502e077f4044befe6
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2013-10-15 19:13:32 +0200

    removed unused imports

--- src/no/whg/GUIMaker/UI/GFrame.java
+++ src/no/whg/GUIMaker/UI/GFrame.java
@@ -1,12 +1,7 @@
 package no.whg.GUIMaker.UI;
 
-import no.whg.GUIMaker.ComponentTable;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 /**
  * Created with IntelliJ IDEA.
